b86cd8405043282f7e6cec2b6b1beba4499ebc05
Nu1Lifier <nolife320@gmail.com>
2020-06-25 21:19:32 +0300

    end

--- src/lesson2/task5.java
+++ src/lesson2/task5.java
@@ -1,4 +1,26 @@
 package lesson2;
 
 public class task5 {
+
+
+
+//    5.** Задать одномерный массив и найти в нем минимальный и максимальный элементы (без помощи интернета);
+//        public static void minAndMax(){
+//            int[] arr = new int[]{4,6,3,7,9,1,2};
+//            int max = 0;
+//            int min = 0;
+//
+//
+//            for (int i=0; i<arr.length; i++){
+//
+//               if (arr[i] > Integer.MIN_VALUE) {min = arr[i];}
+//               if (arr[i] < Integer.MAX_VALUE) {max = arr[i];}
+//            }
+//            System.out.println(min);
+//            System.out.println(max);
+
+
+//    public static void main(String[] args) {
+//
+//    }
 }
