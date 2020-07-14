a8a49c07b560ee7ca60dcd1323fbb76854e0120d
George <64532466+Nu1Lifier@users.noreply.github.com>
2020-07-14 15:29:26 +0300

    add horse class, horse is amazing, u knew that?

--- src/lesson6/animalsApp.java
+++ src/lesson6/animalsApp.java
@@ -4,8 +4,12 @@ public class animalsApp {
     public static void main(String[] args) {
         animal animal = new animal("Busya");
         cat cat = new cat("Bars", "white");
+        horse horse = new horse("Karl");
         animal.animalInfo();
         cat.animalInfo();
         cat.catInfo();
+        horse.horseIsAmazing();
+        horse.animalInfo();
+
     }
 }
--- src/lesson6/horse.java
+++ src/lesson6/horse.java
@@ -1,5 +1,14 @@
 package lesson6;
 
-public class horse {
-    
+public class horse extends animal{
+
+
+    public horse(String name){
+        this.name = name;
+    }
+
+    public void horseIsAmazing(){
+        System.out.println("Look at my hoorse, my " + name + " horse is amaaazing");
+    }
+
 }
