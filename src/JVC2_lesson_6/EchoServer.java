053ff4c8ec41c4a8d24b1de580e17018988031a6
George <64532466+Nu1Lifier@users.noreply.github.com>
2020-08-10 12:50:37 +0300

    add HW

--- src/JVC2_lesson_6/EchoServer.java
+++ src/JVC2_lesson_6/EchoServer.java
@@ -1,29 +1,43 @@
 package JVC2_lesson_6;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.DataInputStream;
+import java.io.DataOutput;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+
+
 public class EchoServer {
+
     public static void main(String[] args) {
         Socket socket = null;
-        try (ServerSocket serverSocket = new ServerSocket(8189)){
+        try (ServerSocket serverSocket = new ServerSocket(8189)) {
             System.out.println("Сервер запущен, ожидаем подключения...");
             socket = serverSocket.accept();
             System.out.println("Клиент подключился");
             DataInputStream in = new DataInputStream(socket.getInputStream());
             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-            while (true){
+            while (true) {
                 String str = in.readUTF();
-                if (str.equals("/end")){
+                if (str.equals("/end")) {
                     break;
                 }
                 out.writeUTF("Echo " + str);
             }
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 }
+
+
+
